eafd89163f050a7fab99439fb90980a890756452
nnajeh <38373885+nnajeh@users.noreply.github.com>
2023-11-13 16:12:55 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -16,3 +16,20 @@ for module in discriminator.modules():
 # Initialize optimizers: pulmonary datasets
 g_optimizer = torch.optim.Adam(generator.parameters(), lr=1e-4, betas=(0.0, 0.999), eps=1e-6)
 d_optimizer = torch.optim.Adam(discriminator.parameters(), lr=4e-4, betas=(0.0, 0.999), eps=1e-6)
+
+
+root = data_pat  # Replace with the actual path to your dataset
+normalize = transforms.Normalize(mean=[0, 0, 0], std=[1, 1, 1])
+
+transform = transforms.Compose([transforms.Resize((128,128)),
+                                               transforms.RandomResizedCrop((128), scale=(0.5, 1.0)),
+                                               transforms.RandomHorizontalFlip(),
+                                               transforms.ToTensor(),
+                                               normalize])
+target_transform = None  # Example target transformation, you can customize it
+
+dataset = ImageFolder(root, transform=transform, target_transform=target_transform)
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+
+

95bc43e83b7ca1727829f3312402cce26714cfea
nnajeh <38373885+nnajeh@users.noreply.github.com>
2023-11-30 04:15:39 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -85,20 +85,8 @@ dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle
         fake_output = discriminator(fake_images.detach(), labels)
         fake_loss = torch.mean(fake_output)
 
-        # Compute gradient penalty
-        alpha = torch.rand(batch_size, 1, 1, 1).to(device)
-        interpolated_images = alpha * real_images + (1 - alpha) * fake_images.detach()
-        interpolated_images.requires_grad_(True)
-        interpolated_output = discriminator(interpolated_images, labels)
-        grad_outputs = torch.ones_like(interpolated_output).to(device)
-        gradients = torch.autograd.grad(outputs=interpolated_output,
-                                        inputs=interpolated_images,
-                                        grad_outputs=grad_outputs,
-                                        create_graph=True,
-                                        retain_graph=True,
-                                        only_inputs=True)[0]
-        gradients = gradients.view(batch_size, -1)
-        gradient_penalty = lambda_gp * ((gradients.norm(2, dim=1) - 1) ** 2).mean()
+        # Compute conditional gradient penalty
+        gradient_penalty = compute_gradient_penalty(D, real_images.data, fake_images.data)
 
         # Update discriminator
         train_d_loss = real_loss + fake_loss + gradient_penalty
